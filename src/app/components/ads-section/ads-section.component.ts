657728bc6f8d892349b697b507fa5a2f8c56bbc0
Hamzahere <hamzahere99@gmail.com>
2023-10-24 02:28:07 +0500

    code refactoring

---
+++ src/app/components/ads-section/ads-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ads-section',
+  templateUrl: './ads-section.component.html',
+  styleUrls: ['./ads-section.component.css']
+})
+export class AdsSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
